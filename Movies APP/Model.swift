92c5fd3253bfdf315f5cd2a48536348f5e66a7d8
Fadysameh2020 <fadysameh10@gmail.com>
2023-05-15 10:24:53 +0300

    first commit

---
+++ Movies APP/Model.swift
@@ -0,0 +1,50 @@
+//
+//  Model.swift
+//  Movies APP
+//
+//  Created by MESHO on 14/05/2023.
+//
+
+import Foundation
+class Movie : Decodable{
+    var id : Int?
+    var vote_average : Int?
+    var title : String?
+    var poster_path : String?
+    var overview : String?
+    var release_date : String?
+}
+
+class MyResult : Decodable{
+    var results : [Movie]?
+    var page: Int?
+    var errorMessage : String?
+}
+
+func fetchResult(complitionHandler: @escaping (MyResult?) -> Void){
+    
+    let url = URL(string: "https://api.themoviedb.org/3/movie/popular?api_key=fdbde11438b838998dd5d98eea5ec77a&language=en-US&page=1")
+    guard let newUrl = url else{
+        return
+    }
+    let request = URLRequest(url: newUrl)
+    let session = URLSession(configuration: .default)
+    
+    let task = session.dataTask(with: request) { (data, response, error) in
+        guard let data = data else{
+            complitionHandler(nil)
+            return
+        }
+        do{
+
+            let result = try JSONDecoder().decode(MyResult.self, from: data)
+            complitionHandler(result)
+            
+        }catch let error{
+            print("error")
+            print(error.localizedDescription)
+            complitionHandler(nil)
+        }
+    }
+    task.resume()
+}
